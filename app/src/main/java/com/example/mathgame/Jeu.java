0190bd32525fb6b2364ff1aea72c24adaa664b95
JosephFlaco <joseph.veys@etud.u-picardie.fr>
2023-03-13 19:26:48 +0100

    MAT-3 (1)

--- app/src/main/java/com/example/mathgame/Jeu.java
+++ app/src/main/java/com/example/mathgame/Jeu.java
@@ -1,4 +1,13 @@
 package com.example.mathgame;
 
-public class Jeu {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Jeu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeu);
+    }
 }
